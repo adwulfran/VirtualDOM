486000d5e89a31a1907367d3ed546cc551ca37ee
adrienw <adrien.wulfran@neurones.net>
2020-09-18 15:51:00 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+import * as wolf from './wolf.js'
+
+function mainComponent() {
+  let h = wolf.h;
+  const a = h("ul", null, h("li", null, "item 1"), h("li", null, "item 2"));
+  const b = h("ul", null, h("li", null, "item 1"), h("li", null, "hello!"));
+const $root = document.getElementById("root");
+const $reload = document.getElementById("reload");
+wolf.updateElement($root, a);
+$reload.addEventListener("click", () => {
+  wolf.updateElement($root, b, a);
+});
+ 
+}
+
+mainComponent();
