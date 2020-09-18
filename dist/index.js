24e75bc3d7aad4efd8c6b3a7da171fdadd2cd05d
adrienw <adrien.wulfran@neurones.net>
2020-09-18 16:54:15 +0200

    split code into files

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,18 @@
-import * as wolf from './wolf.js'
+import * as wolf from "./wolf.js";
 
 function mainComponent() {
   let h = wolf.h;
-  const a = h("ul", null, h("li", null, "item 1"), h("li", null, "item 2"));
+  const a = h("ul", {
+    className: "list",
+    style: "list-style: none;"
+  }, h("li", null, "item 1"), h("li", null, "item 2"));
   const b = h("ul", null, h("li", null, "item 1"), h("li", null, "hello!"));
-const $root = document.getElementById("root");
-const $reload = document.getElementById("reload");
-wolf.updateElement($root, a);
-$reload.addEventListener("click", () => {
-  wolf.updateElement($root, b, a);
-});
- 
+  const $root = document.getElementById("root");
+  const $reload = document.getElementById("reload");
+  wolf.updateElement($root, a);
+  $reload.addEventListener("click", () => {
+    wolf.updateElement($root, b, a);
+  });
 }
 
 mainComponent();
